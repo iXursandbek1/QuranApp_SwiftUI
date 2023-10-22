b508502bb3dc8095ad5718238f2e143aee85ccfb
Xursandbek <xursandbek06082001@mail.com>
2023-10-22 21:38:06 +0500

    Tab bar added.

---
+++ QuranApp_SwiftUI/Screens/Holy_Quran_View.swift
@@ -0,0 +1,18 @@
+//
+//  Holy_Quran_View.swift
+//  QuranApp_SwiftUI
+//
+//  Created by Xursandbek Qambaraliyev on 20/10/23.
+//
+
+import SwiftUI
+
+struct Holy_Quran_View: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Holy_Quran_View()
+}
---
+++ QuranApp_SwiftUI/Screens/QuranAppTabView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  QuranApp_SwiftUI
+//
+//  Created by Xursandbek Qambaraliyev on 20/10/23.
+//
+
+import SwiftUI
+
+struct QuranAppTabView: View {
+   
+    var body: some View {
+        
+    }
+
+}
+
+#Preview {
+    QuranAppTabView()
+}
