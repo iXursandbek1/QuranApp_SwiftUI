f52b2c4b5479d8feb7ee43a68a6d53732475f059
Xursandbek <xursandbek06082001@mail.com>
2023-10-31 14:04:52 +0500

    Surah and Ayah views

---
+++ QuranApp_SwiftUI/Screens/AyahListView/AyahListView.swift
@@ -0,0 +1,18 @@
+//
+//  AyahListView.swift
+//  QuranApp_SwiftUI
+//
+//  Created by Xursandbek Qambaraliyev on 31/10/23.
+//
+
+import SwiftUI
+
+struct AyahListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AyahListView()
+}
---
+++ QuranApp_SwiftUI/Views/Cells/AyahListCell.swift
@@ -0,0 +1,18 @@
+//
+//  AyahListCell.swift
+//  QuranApp_SwiftUI
+//
+//  Created by Xursandbek Qambaraliyev on 31/10/23.
+//
+
+import SwiftUI
+
+struct AyahListCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AyahListCell()
+}
